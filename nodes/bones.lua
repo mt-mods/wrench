8590d7ba42ce9da1b04933a4b15480ca5337263f
nixnoxus <nixnoxus@web.de>
2021-11-29 18:20:35 +0100

    add bones support

---
+++ nodes/bones.lua
@@ -0,0 +1,13 @@
+
+-- Register wrench support for bones
+
+wrench.register_node("bones:bones", {
+	lists = {"main"},
+        metas = {
+                owner = wrench.META_TYPE_STRING,
+                infotext = wrench.META_TYPE_STRING,
+                formspec = wrench.META_TYPE_STRING,
+                time = wrench.META_TYPE_INT,
+        },
+        owned = true,
+})

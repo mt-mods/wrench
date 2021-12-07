220d93786f07c9051df7b5a627d661455ef2492b
nixnoxus <nixnoxus@web.de>
2021-12-07 16:30:03 +0100

    split `mesecons_*`

---
+++ nodes/mesecons_detector.lua
@@ -0,0 +1,25 @@
+
+-- Register wrench support for mesecons_detector
+
+local states = { "off", "on" }
+for _, state in ipairs(states) do
+	wrench.register_node("mesecons_detector:node_detector_"..state, {
+		drop = state == "on",
+		metas = {
+			formspec = wrench.META_TYPE_STRING,
+			distance = wrench.META_TYPE_STRING,
+			digiline_channel = wrench.META_TYPE_STRING,
+			scanname = wrench.META_TYPE_STRING,
+		},
+		description = wrench.description_with_configuration,
+	})
+	wrench.register_node("mesecons_detector:object_detector_"..state, {
+		drop = state == "on",
+		metas = {
+			formspec = wrench.META_TYPE_STRING,
+			digiline_channel = wrench.META_TYPE_STRING,
+			scanname = wrench.META_TYPE_STRING,
+		},
+		description = wrench.description_with_configuration,
+	})
+end

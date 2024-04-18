4aade586d39f114bcc2e0352d5460c8e290be453
OgelGames <olliverdc28@gmail.com>
2024-04-18 19:25:50 +1000

    add `can_pickup` for custom protection checks

--- nodes/bones.lua
+++ nodes/bones.lua
@@ -1,7 +1,24 @@
 
 -- Register wrench support for bones
 
+local S = wrench.translator
+
 if bones.redo then
+	local function can_pickup_bones(pos, player)
+		local meta = minetest.get_meta(pos)
+		if meta:get_string("infotext") == "" then
+			return false
+		end
+		if meta:get_int("time") >= bones.share_time then
+			return true
+		end
+		local name = player:get_player_name()
+		local owner = meta:get_string("owner")
+		if owner == "" or owner == name or minetest.check_player_privs(name, "protection_bypass") then
+			return true
+		end
+		return false, S("Cannot pickup node. Owned by @1.", owner)
+	end
 	wrench.register_node("bones:bones", {
 		lists = {"main"},
 		metas = {
@@ -9,8 +26,8 @@ if bones.redo then
 			infotext = wrench.META_TYPE_STRING,
 			time = wrench.META_TYPE_INT,
 		},
-		owned = true,
 		timer = true,
+		can_pickup = can_pickup_bones,
 	})
 else
 	wrench.register_node("bones:bones", {

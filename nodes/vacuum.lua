5d0519687d73a9404c8bf304faa1847b8d2477a1
Luke aka SwissalpS <Luke@SwissalpS.ws>
2024-12-26 17:58:14 +0100

    infotext can be ignored

--- nodes/vacuum.lua
+++ nodes/vacuum.lua
@@ -6,7 +6,7 @@ wrench.register_node("vacuum:airpump", {
 	metas = {
 		enabled = wrench.META_TYPE_INT,
 		formspec = wrench.META_TYPE_IGNORE,
-		infotext = wrench.META_TYPE_STRING,
+		infotext = wrench.META_TYPE_IGNORE,
 		owner = wrench.META_TYPE_STRING,
 	},
 })

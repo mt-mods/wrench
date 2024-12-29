aa0b93a1d27a736acb31aabeaeb5919417407899
Luke aka SwissalpS <Luke@SwissalpS.ws>
2024-12-29 19:09:47 +0100

    use exposed cannon definitions

--- nodes/spacecannon.lua
+++ nodes/spacecannon.lua
@@ -1,15 +1,9 @@
 
 -- Register wrench support for the spacecannon mod
 
-for colour, ballistic in pairs({
-		blue = true,
-		green = false,
-		purple = true,
-		red = false,
-		yellow = false,
-	}) do
-	wrench.register_node("spacecannon:cannon_" .. colour, {
-		lists = ballistic and { "src" } or {},
+for _, def in ipairs(spacecannon.cannon_defs) do
+	wrench.register_node("spacecannon:cannon_" .. def.color, {
+		lists = def.is_th and {} or { "src" },
 		metas = {
 			channel = wrench.META_TYPE_STRING,
 			formspec = wrench.META_TYPE_IGNORE,
